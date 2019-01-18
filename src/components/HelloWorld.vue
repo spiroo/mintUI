dadb103c4464607a19552b76ab53ee414ad9cd2a
spiroo <spiroo@126.com>
2019-01-18 14:46:38 +0800

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      <mt-button type="default">default</mt-button>
+      <mt-button type="primary">primary</mt-button>
+      <mt-button type="danger">danger</mt-button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <mt-header title="标题过长会隐藏后面的内容啊哈哈哈哈">
+      <router-link to="/" slot="left">
+        <mt-button icon="back">返回</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+  </div>
+</template>
